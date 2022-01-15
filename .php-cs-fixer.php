43920f1825750d0082ea62cba50b10005f7d3809
Louis Charette <charette.louis@gmail.com>
2022-01-15 14:44:05 -0500

    Update header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,7 @@
 $header = 'UserFrosting Account Sprinkle (http://www.userfrosting.com)
 
 @link      https://github.com/userfrosting/sprinkle-account
-@copyright Copyright (c) 2021 Alexander Weissman & Louis Charette
+@copyright Copyright (c) 2022 Alexander Weissman & Louis Charette
 @license   https://github.com/userfrosting/sprinkle-account/blob/master/LICENSE.md (MIT License)';
 
 $rules = [
