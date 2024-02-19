5cc867bd1c52416276471e8d20226265a75ca4a9
Louis Charette <charette.louis@gmail.com>
2024-02-18 21:06:26 -0500

    Add Missing default permission Fix https://github.com/userfrosting/UserFrosting/issues/1225

--- app/src/Database/Seeds/DefaultPermissions.php
+++ app/src/Database/Seeds/DefaultPermissions.php
@@ -50,31 +50,31 @@ class DefaultPermissions implements SeedInterface
         ];
 
         return [
-            'create_group'            => new Permission([
+            'create_group' => new Permission([
                 'slug'        => 'create_group',
                 'name'        => 'Create group',
                 'conditions'  => 'always()',
                 'description' => 'Create a new group.',
             ]),
-            'create_user'             => new Permission([
+            'create_user' => new Permission([
                 'slug'        => 'create_user',
                 'name'        => 'Create user',
                 'conditions'  => 'always()',
                 'description' => 'Create a new user in your own group and assign default roles.',
             ]),
-            'create_user_field'       => new Permission([
+            'create_user_field' => new Permission([
                 'slug'        => 'create_user_field',
                 'name'        => 'Set new user group',
                 'conditions'  => "subset(fields,['group'])",
                 'description' => 'Set the group when creating a new user.',
             ]),
-            'delete_group'            => new Permission([
+            'delete_group' => new Permission([
                 'slug'        => 'delete_group',
                 'name'        => 'Delete group',
                 'conditions'  => 'always()',
                 'description' => 'Delete a group.',
             ]),
-            'delete_user'             => new Permission([
+            'delete_user' => new Permission([
                 'slug'        => 'delete_user',
                 'name'        => 'Delete user',
                 'conditions'  => "!has_role(user.id,{$defaultRoleIds['site-admin']}) && !is_master(user.id)",
@@ -86,13 +86,13 @@ class DefaultPermissions implements SeedInterface
                 'conditions'  => 'always()',
                 'description' => 'Edit your own account settings.',
             ]),
-            'update_group_field'      => new Permission([
+            'update_group_field' => new Permission([
                 'slug'        => 'update_group_field',
                 'name'        => 'Edit group',
                 'conditions'  => 'always()',
                 'description' => 'Edit basic properties of any group.',
             ]),
-            'update_user_field'       => new Permission([
+            'update_user_field' => new Permission([
                 'slug'        => 'update_user_field',
                 'name'        => 'Edit user',
                 'conditions'  => "!has_role(user.id,{$defaultRoleIds['site-admin']}) && subset(fields,['name','email','locale','group','flag_enabled','flag_verified','password'])",
@@ -104,79 +104,103 @@ class DefaultPermissions implements SeedInterface
                 'conditions'  => "equals_num(self.group_id,user.group_id) && !is_master(user.id) && !has_role(user.id,{$defaultRoleIds['site-admin']}) && (!has_role(user.id,{$defaultRoleIds['group-admin']}) || equals_num(self.id,user.id)) && subset(fields,['name','email','locale','flag_enabled','flag_verified','password'])",
                 'description' => 'Edit users in your own group who are not Site or Group Administrators, except yourself.',
             ]),
-            'uri_account_settings'    => new Permission([
+            'uri_account_settings' => new Permission([
                 'slug'        => 'uri_account_settings',
                 'name'        => 'Account settings page',
                 'conditions'  => 'always()',
                 'description' => 'View the account settings page.',
             ]),
-            'uri_activities'          => new Permission([
+            'uri_activities' => new Permission([
                 'slug'        => 'uri_activities',
                 'name'        => 'Activity monitor',
                 'conditions'  => 'always()',
                 'description' => 'View a list of all activities for all users.',
             ]),
-            'uri_dashboard'           => new Permission([
+            'uri_dashboard' => new Permission([
                 'slug'        => 'uri_dashboard',
                 'name'        => 'Admin dashboard',
                 'conditions'  => 'always()',
                 'description' => 'View the administrative dashboard.',
             ]),
-            'uri_group'               => new Permission([
+            'uri_group' => new Permission([
                 'slug'        => 'uri_group',
                 'name'        => 'View group',
                 'conditions'  => 'always()',
                 'description' => 'View the group page of any group.',
             ]),
-            'uri_group_own'           => new Permission([
+            'uri_group_own' => new Permission([
                 'slug'        => 'uri_group',
                 'name'        => 'View own group',
                 'conditions'  => 'equals_num(self.group_id,group.id)',
                 'description' => 'View the group page of your own group.',
             ]),
-            'uri_groups'              => new Permission([
+            'uri_groups' => new Permission([
                 'slug'        => 'uri_groups',
                 'name'        => 'Group management page',
                 'conditions'  => 'always()',
                 'description' => 'View a page containing a list of groups.',
             ]),
-            'uri_user'                => new Permission([
+            'uri_permissions' => new Permission([
+                'slug'        => 'uri_permissions',
+                'name'        => 'Permission management page',
+                'conditions'  => 'always()',
+                'description' => 'View a page containing a list of permissions.',
+            ]),
+            'uri_role' => new Permission([
+                'slug'        => 'uri_role',
+                'name'        => 'View role',
+                'conditions'  => 'always()',
+                'description' => 'View the role page of any role.',
+            ]),
+            'uri_roles' => new Permission([
+                'slug'        => 'uri_roles',
+                'name'        => 'Role management page',
+                'conditions'  => 'always()',
+                'description' => 'View a page containing a list of roles.',
+            ]),
+            'uri_user' => new Permission([
                 'slug'        => 'uri_user',
                 'name'        => 'View user',
                 'conditions'  => 'always()',
                 'description' => 'View the user page of any user.',
             ]),
-            'uri_user_in_group'       => new Permission([
+            'uri_user_in_group' => new Permission([
                 'slug'        => 'uri_user',
                 'name'        => 'View user',
                 'conditions'  => "equals_num(self.group_id,user.group_id) && !is_master(user.id) && !has_role(user.id,{$defaultRoleIds['site-admin']}) && (!has_role(user.id,{$defaultRoleIds['group-admin']}) || equals_num(self.id,user.id))",
                 'description' => 'View the user page of any user in your group, except the master user and Site and Group Administrators (except yourself).',
             ]),
-            'uri_users'               => new Permission([
+            'uri_users' => new Permission([
                 'slug'        => 'uri_users',
                 'name'        => 'User management page',
                 'conditions'  => 'always()',
                 'description' => 'View a page containing a table of users.',
             ]),
-            'view_group_field'        => new Permission([
+            'view_group_field' => new Permission([
                 'slug'        => 'view_group_field',
                 'name'        => 'View group',
                 'conditions'  => "in(property,['name','icon','slug','description','users'])",
                 'description' => 'View certain properties of any group.',
             ]),
-            'view_group_field_own'    => new Permission([
+            'view_group_field_own' => new Permission([
                 'slug'        => 'view_group_field',
                 'name'        => 'View group',
                 'conditions'  => "equals_num(self.group_id,group.id) && in(property,['name','icon','slug','description','users'])",
                 'description' => 'View certain properties of your own group.',
             ]),
-            'view_user_field'         => new Permission([
+            'view_role_field' => new Permission([
+                'slug'        => 'view_role_field',
+                'name'        => 'View role',
+                'conditions'  => "in(property,['name','slug','description','permissions'])",
+                'description' => 'View certain properties of any role.',
+            ]),
+            'view_user_field' => new Permission([
                 'slug'        => 'view_user_field',
                 'name'        => 'View user',
                 'conditions'  => "in(property,['user_name','name','email','locale','theme','roles','group','activities'])",
                 'description' => 'View certain properties of any user.',
             ]),
-            'view_user_field_group'   => new Permission([
+            'view_user_field_group' => new Permission([
                 'slug'        => 'view_user_field',
                 'name'        => 'View user',
                 'conditions'  => "equals_num(self.group_id,user.group_id) && !is_master(user.id) && !has_role(user.id,{$defaultRoleIds['site-admin']}) && (!has_role(user.id,{$defaultRoleIds['group-admin']}) || equals_num(self.id,user.id)) && in(property,['user_name','name','email','locale','roles','group','activities'])",
