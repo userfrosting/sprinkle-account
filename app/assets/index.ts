fcf1d17e4f757713ea26efcc8b1bf2f9e33b49e7
Louis Charette <charette.louis@gmail.com>
2025-03-25 21:54:47 -0400

    Register $checkAccess globally

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -3,15 +3,29 @@ import type { Router } from 'vue-router'
 import { useAuthStore } from './stores/auth'
 import { useAuthGuard } from './guards/authGuard'
 
-/* Install plugins */
+/**
+ * Account Sprinkle initialization recipe.
+ *
+ * This recipe is responsible for running the auth check on load, setting up
+ * the router guards and registering the checkAccess as a global properties.
+ */
 export default {
     install: (app: App, options: { router: Router }) => {
-        // Run auth check on load
+        /**
+         * Run auth check on load
+         */
         const auth = useAuthStore()
         auth.check()
 
-        // Setup router guards
+        /**
+         * Setup router guards
+         */
         const { router } = options
         useAuthGuard(router)
+
+        /**
+         * Register checkAccess as a global property
+         */
+        app.config.globalProperties.$checkAccess = auth.checkAccess
     }
 }
