1a62eda6a8eb93680c635e623a1b1d17fcf9aa19
Louis Charette <charette.louis@gmail.com>
2025-08-18 21:15:46 -0400

    Implement password validation

--- app/assets/composables/useUserPasswordEditApi.ts
+++ app/assets/composables/useUserPasswordEditApi.ts
@@ -1,22 +1,41 @@
 import { ref } from 'vue'
 import axios from 'axios'
+import { useRegle } from '@regle/core'
 import {
     type ApiResponse,
     type ApiErrorResponse,
     Severity
 } from '@userfrosting/sprinkle-core/interfaces'
 import type { PasswordEditRequest } from '../interfaces'
-import { useAlertsStore } from '@userfrosting/sprinkle-core/stores'
-
-// TODO : Add validation
-// 'schema://requests/account-settings.yaml'
+import { useAlertsStore, useConfigStore } from '@userfrosting/sprinkle-core/stores'
+import { useRuleSchemaAdapter } from '@userfrosting/sprinkle-core/composables'
+import schemaFile from '../../schema/requests/account-settings.yaml?raw'
 
 /**
  * API Composable
  */
 export function useUserPasswordEditApi() {
-    const apiLoading = ref<Boolean>(false)
+    const apiLoading = ref<boolean>(false)
     const apiError = ref<ApiErrorResponse | null>(null)
+    const passwordMinLength = ref<number>(0)
+    const passwordMaxLength = ref<number>(0)
+    const formData = ref<PasswordEditRequest>({
+        passwordcheck: '',
+        password: '',
+        passwordc: ''
+    })
+
+    // Retrieve min/max password length from site settings and update validator
+    // constraints
+    const config = useConfigStore()
+    passwordMinLength.value = config.get('site.password.length.min')
+    passwordMaxLength.value = config.get('site.password.length.max')
+
+    // TODO : Pass min/max to Regle (can't change defined regle, the message won't follow)
+    // TODO : matches rules is not implemented
+
+    // Load the schema and set up the validator
+    const { r$ } = useRegle(formData, useRuleSchemaAdapter().adapt(schemaFile))
 
     async function submitPasswordEdit(data: PasswordEditRequest) {
         apiLoading.value = true
@@ -40,5 +59,13 @@ export function useUserPasswordEditApi() {
             })
     }
 
-    return { submitPasswordEdit, apiLoading, apiError }
+    return {
+        submitPasswordEdit,
+        apiLoading,
+        apiError,
+        formData,
+        r$,
+        minLength: passwordMinLength,
+        maxLength: passwordMaxLength
+    }
 }
