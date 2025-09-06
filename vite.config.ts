13a665ec7397a3f1c7b73aa51157ca6cad31d866
Louis Charette <charette.louis@gmail.com>
2025-09-06 15:38:19 -0400

    Fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 import { configDefaults } from 'vitest/config'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteYaml from '@modyfi/vite-plugin-yaml'
 
 // https://vitejs.dev/config/
 // https://stackoverflow.com/a/74397545/445757
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), ViteYaml()],
     test: {
         coverage: {
             reportsDirectory: './_meta/_coverage',
