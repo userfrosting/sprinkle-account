04cd5193a5c1f49edd148f5399c18a8b5605c891
Louis Charette <charette.louis@gmail.com>
2026-01-10 18:08:31 -0500

    Add resolve conditions for monorepo support in Vite configurations and introduce Vitest configuration for theme-pink-cupcake

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 // https://stackoverflow.com/a/74397545/445757
 export default defineConfig({
+    resolve: {
+        conditions: ['userfrosting:monorepo', 'import']
+    },
     plugins: [
         vue(),
         ViteYaml(),
